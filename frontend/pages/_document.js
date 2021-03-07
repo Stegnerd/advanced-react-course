fe766a0a5b22a88b3529043a1af638536dc11f2c
Dalton Stegner <dmstegner@gmail.com>
2021-03-07 12:58:29 -0500

    module 3 section 9 inital props flicker

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,19 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    // go through every single component and looks for styled components css
+    // rips it out and renders it to the server
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    // gathers all styled components css and applies it to page
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>
