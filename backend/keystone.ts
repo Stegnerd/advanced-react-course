d5eb96cbecbba0ca35bc6e04548427242fe7f1dc
Dalton Stegner <dmstegner@gmail.com>
2021-03-07 14:12:16 -0500

    module 4 section 12 setting up keystone config

---
+++ backend/keystone.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import { config, createSchema} from '@keystone-next/keystone/schema'
+
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  // jwt auth ttl
+  maxAge: 60 * 60 * 24  * 360, // how long should they stay signed in
+  secret: process.env.COOKIE_SECRET
+};
+
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO ADD DATA SEEDING HERE
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO: Change this for roles
+    isAccessAllowed: () => true
+  },
+  // TODO: add session values here
+});
