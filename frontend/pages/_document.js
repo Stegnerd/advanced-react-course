0297f4e9ebc00da7411ebed1cc8ed98ce1ea615c
Dalton Stegner <dmstegner@gmail.com>
2021-03-07 12:48:54 -0500

    Module 3 section 8 nprogress

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        {/* <head></head> */}
+        <Head />
         <body>
           <Main />
           <NextScript />
