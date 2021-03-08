5445cdb0c8c69ad96afd26480c5d86def92a6c19
Dalton Stegner <dmstegner@gmail.com>
2021-03-07 22:29:51 -0500

    Module4 Section 14 added auth to the backend

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,35 +1,57 @@
 import 'dotenv/config';
-import { config, createSchema} from '@keystone-next/keystone/schema'
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
   // jwt auth ttl
-  maxAge: 60 * 60 * 24  * 360, // how long should they stay signed in
-  secret: process.env.COOKIE_SECRET
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
+  secret: process.env.COOKIE_SECRET,
 };
 
-
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  // which object is responsible for auth
+  listKey: 'User',
+  // which field identitfies user
+  identityField: 'email',
+  secretField: 'password',
+  // allows first person logging to seed themselves as a user
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add initial roles
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO ADD DATA SEEDING HERE
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User
-  }),
-  ui: {
-    // TODO: Change this for roles
-    isAccessAllowed: () => true
-  },
-  // TODO: add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO ADD DATA SEEDING HERE
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // show ui only for people who pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
