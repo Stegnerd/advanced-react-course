948a32b01cc0df58d254808d255a66807e28d221
Dalton Stegner <dmstegner@gmail.com>
2021-03-14 21:16:15 -0400

    module5 section 19 setting up apollo client

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -8,6 +8,7 @@ import { endpoint, prodEndpoint } from '../config';
 function createClient({ headers, initialState }) {
   return new ApolloClient({
     link: ApolloLink.from([
+      // create error handler error for if you do something wrong with graphql or network issues
       onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
           graphQLErrors.forEach(({ message, locations, path }) =>
@@ -23,13 +24,15 @@ function createClient({ headers, initialState }) {
       // this uses apollo-link-http under the hood, so all the options here come from that package
       createUploadLink({
         uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        // pass along cookies because we need to make sure users are logged in before they do actions
         fetchOptions: {
           credentials: 'include',
         },
-        // pass the headers along from this request. This enables SSR with logged in state
+        // pass the headers along from this request. This enables server side rendering with logged in state
         headers,
       }),
     ]),
+    // in browser cache for storing query results from graph
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
@@ -43,4 +46,5 @@ function createClient({ headers, initialState }) {
   });
 }
 
+// getDataFromTree is a helper that crawls the website and looks for queries and mutations
 export default withApollo(createClient, { getDataFromTree });
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,20 +2,37 @@
 
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 
 // override nprogress styling by importings our own with the same name
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 // hook into router to trigger things programmatically
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// specific NextJS method to get apollo queries per page
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+// this helper creates an apollo client and passes it into the MyApp
+export default withData(MyApp);
