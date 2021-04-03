cf0902885133f0c81cd520254076505dd505ed06
Dalton Stegner <dmstegner@gmail.com>
2021-04-03 01:07:13 -0400

    Module 5 section 21

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -27,7 +27,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -44,11 +44,11 @@ export default function Header() {
         <LogoStyles>
           <Link href="/">Sick Fits</Link>
         </LogoStyles>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
